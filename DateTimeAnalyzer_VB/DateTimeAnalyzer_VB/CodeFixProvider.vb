1a214cf40d656bb180ac17148f231aae7afb4f84
AlessandroDelSole <alessandro.delsole@visual-basic.it>
2015-09-11 22:08:19 +0200

    Refactored VB analyzer + indentation fix for edit

--- DateTimeAnalyzer_VB/DateTimeAnalyzer_VB/CodeFixProvider.vb
+++ DateTimeAnalyzer_VB/DateTimeAnalyzer_VB/CodeFixProvider.vb
@@ -40,41 +40,13 @@ Public Class DateTimeAnalyzer_VBCodeFixProvider
 
         'Register a code action that invokes the fix
         'on the current document
-        If TypeOf (node) Is PredefinedTypeSyntax Then
-            context.RegisterCodeFix(
-            CodeAction.
-            Create("Replace with DateTimeOffset",
-                   Function(c) ReplaceDateAsync(context.Document,
-                              node, c),
-                              equivalenceKey:=title),
-                              diagnostic)
-        Else
-            context.RegisterCodeFix(
-            CodeAction.
-            Create("Replace with DateTimeOffset",
-                   Function(c) ReplaceDateTimeAsync(context.Document,
-                              node, c),
-                              equivalenceKey:=title),
-                              diagnostic)
-        End If
-    End Function
-
-    Private Async Function _
-        ReplaceDateAsync(Document As Document,
-                         node As PredefinedTypeSyntax,
-                         CancellationToken As CancellationToken) _
-                         As Task(Of Document)
-
-        Dim root = Await Document.GetSyntaxRootAsync
-        Dim generator = SyntaxGenerator.GetGenerator(Document)
-
-        Dim newIdentifierSyntax =
-            generator.IdentifierName("DateTimeOffset").
-            WithTrailingTrivia(node.GetTrailingTrivia)
-
-        Dim newRoot = root.ReplaceNode(node, newIdentifierSyntax)
-        Dim newDocument = Document.WithSyntaxRoot(newRoot)
-        Return newDocument
+        context.RegisterCodeFix(
+        CodeAction.
+        Create("Replace with DateTimeOffset",
+               Function(c) ReplaceDateTimeAsync(context.Document,
+                          node, c),
+                          equivalenceKey:=title),
+                          diagnostic)
     End Function
 
     Private Async Function ReplaceDateTimeAsync _
@@ -86,20 +58,33 @@ Public Class DateTimeAnalyzer_VBCodeFixProvider
         'Get the root syntax node for the current document
         Dim root = Await document.GetSyntaxRootAsync
 
+        Dim newRoot As SyntaxNode = Nothing
+
         'Convert the syntax node into the specialized kind
-        Dim convertedNode = DirectCast(node, IdentifierNameSyntax)
+        If TypeOf (node) Is IdentifierNameSyntax Then
+            Dim convertedNode = DirectCast(node, IdentifierNameSyntax)
 
-        'Create a new syntax node
-        Dim newNode = convertedNode.
-                      WithIdentifier(SyntaxFactory.
-                      ParseToken("DateTimeOffset")).
-                      WithTrailingTrivia(node.
-                      GetTrailingTrivia)
+            'Create a new syntax node
+            Dim newNode = convertedNode.
+                          WithIdentifier(SyntaxFactory.
+                          ParseToken("DateTimeOffset")).
+                          WithTrailingTrivia(node.
+                          GetTrailingTrivia)
 
-        'Create a New root syntax node for the current document
-        'replacing the syntax node that has diagnostic with
-        'a new syntax node
-        Dim newRoot = root.ReplaceNode(node, newNode)
+            'Create a New root syntax node for the current document
+            'replacing the syntax node that has diagnostic with
+            'a new syntax node
+            newRoot = root.ReplaceNode(node, newNode)
+        ElseIf TypeOf (node) Is PredefinedTypeSyntax
+            Dim convertedNode = DirectCast(node, PredefinedTypeSyntax)
+
+            Dim newIdentifierName = SyntaxFactory.
+                          IdentifierName(SyntaxFactory.
+                          ParseToken("DateTimeOffset")).
+                          WithTrailingTrivia(node.
+                          GetTrailingTrivia)
+            newRoot = root.ReplaceNode(node, newIdentifierName)
+        End If
 
         'Generate a new document
         Dim newDocument = document.WithSyntaxRoot(newRoot)
