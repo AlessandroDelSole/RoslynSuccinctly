ae492b615357ab2f99e430f4efaeec0096503e67
AlessandroDelSole <alessandro.delsole@visual-basic.it>
2015-09-13 18:26:19 +0200

    Generally fixed indentation for the book

--- PublicFieldRefactoring/PublicFieldRefactoring/CodeRefactoringProvider.cs
+++ PublicFieldRefactoring/PublicFieldRefactoring/CodeRefactoringProvider.cs
@@ -17,11 +17,11 @@ namespace PublicFieldRefactoring
         Name = nameof(PublicFieldRefactoringCodeRefactoringProvider)), Shared]
     internal class PublicFieldRefactoringCodeRefactoringProvider : CodeRefactoringProvider
     {
-        public sealed override async Task ComputeRefactoringsAsync(CodeRefactoringContext context)
+        public sealed override async Task 
+            ComputeRefactoringsAsync(
+            CodeRefactoringContext context)
         {
-            // TODO: Replace the following code with your own analysis, 
-            // generating a CodeAction for each refactoring to offer
-
+            // Get the root syntax node
             var root = await context.Document.
                 GetSyntaxRootAsync(context.CancellationToken).
                 ConfigureAwait(false);
@@ -29,13 +29,19 @@ namespace PublicFieldRefactoring
             // Find the node at the selection.
             var node = root.FindNode(context.Span);
 
+            // Convert the node into a field declaration
             var fieldDecl = node as FieldDeclarationSyntax;
+            // If it's not public, return
             if (fieldDecl == null || 
                 fieldDecl.Modifiers.ToFullString().
                 Contains("public") == false) { return; }
 
+            // Used to determine if an action must
+            // be registered
             bool mustRegisterAction = false;
 
+            // If at least one starting character is
+            // lower case, must register an action
             if (fieldDecl.Declaration.
                 Variables.Any(i => 
                 char.IsLower(i.Identifier.
@@ -54,10 +60,15 @@ namespace PublicFieldRefactoring
                 return;
             }
 
-            var action = CodeAction.Create("Make first char upper case",
-                               c => RenameFieldAsync(context.
-                               Document, fieldDecl, c));
+            // Create an action invoking a delegate passing
+            // the document instance, the syntax node, and
+            // a cancellation token
+            var action = 
+                CodeAction.Create("Make first char upper case",
+                           c => RenameFieldAsync(context.
+                           Document, fieldDecl, c));
 
+            // Register this code action
             context.RegisterRefactoring(action);       
         }
 
@@ -66,45 +77,66 @@ namespace PublicFieldRefactoring
             FieldDeclarationSyntax fieldDeclaration, 
             CancellationToken cancellationToken)
         {
+            // Get the semantic model for the code file
             var semanticModel = 
                 await document.
                       GetSemanticModelAsync(cancellationToken).
                       ConfigureAwait(false);
 
+            // Get the root syntax node
             var root = await document.GetSyntaxRootAsync();
 
+            // Get a list of old variable declarations
             var oldDeclarators = 
                 fieldDeclaration.Declaration.
                 Variables;
 
+            // Store the collection of variables
             var listOfNewModifiedDeclarators = 
-                new SeparatedSyntaxList<VariableDeclaratorSyntax>();
+                new SeparatedSyntaxList<
+                    VariableDeclaratorSyntax>();
 
+            // Iterate the declarators collection
             foreach (var declarator in oldDeclarators) {
+                // Get a new name
                 var tempString = 
                     ConvertName(declarator.Identifier.
                     ToFullString());
 
+                // Generate a new modified declarator
+                // based on the previous one but with
+                // a new identifier
                 listOfNewModifiedDeclarators = 
                     listOfNewModifiedDeclarators.
                     Add(declarator.WithIdentifier(
                         SyntaxFactory.ParseToken(tempString)));
             }
 
+            // Generate a new field declaration
+            // with updated variable names
             var newDeclaration = 
                 fieldDeclaration.Declaration.
                 WithVariables(listOfNewModifiedDeclarators);
 
+            // Generate a new FieldDeclarationSyntax
             var newField = fieldDeclaration.
                 WithDeclaration(newDeclaration);
 
+            // Replace the old syntax node with the new one
             SyntaxNode newRoot = root.
-                       ReplaceNode(fieldDeclaration, newField);
-            var newDocument = document.WithSyntaxRoot(newRoot);
+                       ReplaceNode(fieldDeclaration, 
+                       newField);
 
+            // Generate a new document
+            var newDocument = 
+                document.WithSyntaxRoot(newRoot);
+
+            // Return the document
             return newDocument;
        }
 
+        // Return a new identifier with an
+        // uppercase letter
         private string ConvertName(string oldName)
         {
             return char.
--- PublicFieldRefactoring_VB/PublicFieldRefactoring_VB/CodeRefactoringProvider.vb
+++ PublicFieldRefactoring_VB/PublicFieldRefactoring_VB/CodeRefactoringProvider.vb
@@ -2,27 +2,41 @@
 Friend Class PublicFieldRefactoring_VBCodeRefactoringProvider
     Inherits CodeRefactoringProvider
 
-    Public NotOverridable Overrides Async Function ComputeRefactoringsAsync(context As CodeRefactoringContext) As Task
-        ' TODO: Replace the following code with your own analysis, generating a CodeAction for each refactoring to offer
+    Public NotOverridable Overrides Async _
+        Function ComputeRefactoringsAsync(
+                 context As CodeRefactoringContext) As Task
 
+        'Get the root syntax node
         Dim root = Await context.Document.
-            GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(False)
+            GetSyntaxRootAsync(context.
+            CancellationToken).ConfigureAwait(False)
 
         ' Find the node at the selection.
         Dim node = root.FindNode(context.Span)
 
-        Dim fieldDecl = TryCast(node, FieldDeclarationSyntax)
+        'Convert the node to a field declaration
+        Dim fieldDecl = TryCast(node,
+            FieldDeclarationSyntax)
+
+        'If it's not Public, return
         If fieldDecl Is Nothing Or fieldDecl.Modifiers.
             ToFullString.Contains("Public") = False Then
             Return
         End If
 
+        'Used to determine if an action
+        'must be registered
         Dim mustRegisterAction As Boolean
 
-        For Each declarator In fieldDecl.Declarators
-            'If at least one starting character is upper case, must register an action 
+        'Iterate the variable declarators
+        For Each declarator
+            In fieldDecl.Declarators
+
+            'If at least one starting character 
+            'is lower case, must register an action 
             If declarator.Names.Any(Function(d) _
-                                        Char.IsLower(d.Identifier.Value.ToString(0))) Then
+                          Char.IsLower(d.Identifier.
+                          Value.ToString(0))) Then
                 mustRegisterAction = True
             Else
                 mustRegisterAction = False
@@ -32,49 +46,69 @@ Friend Class PublicFieldRefactoring_VBCodeRefactoringProvider
         If mustRegisterAction = False Then
             Return
         Else
-            Dim action = CodeAction.Create("Make first char upper case",
-                                           Function(c) RenameFieldAsync(context.
-                                           Document, fieldDecl, c))
+            'Create an action invoking a delegate passing
+            'the document instance, the syntax node, and
+            'a cancellation token
+            Dim action =
+                CodeAction.Create("Make first char upper case",
+                           Function(c) RenameFieldAsync(context.
+                           Document, fieldDecl, c))
 
             ' Register this code action.
             context.RegisterRefactoring(action)
         End If
     End Function
 
-    Private Async Function RenameFieldAsync(document As Document,
-                                            fieldDeclaration As FieldDeclarationSyntax,
-                                        cancellationToken As CancellationToken) _
-                                        As Task(Of Document)
+    Private Async Function _
+        RenameFieldAsync(document As Document,
+                         fieldDeclaration As FieldDeclarationSyntax,
+                         cancellationToken As CancellationToken) _
+                         As Task(Of Document)
 
+        'Get the semantic model for the code file
         Dim semanticModel = Await document.
             GetSemanticModelAsync(cancellationToken).
             ConfigureAwait(False)
 
+        'Get the root syntax node
         Dim root = Await document.GetSyntaxRootAsync
 
-        Dim oldDeclarators = fieldDeclaration.Declarators
+        'Get a list of old declarators
+        Dim oldDeclarators =
+            fieldDeclaration.Declarators
+
+        'Store the collection of identifiers
         Dim listOfNewModifiedIdentifiers As _
-            New SeparatedSyntaxList(Of ModifiedIdentifierSyntax)
+            New SeparatedSyntaxList(
+            Of ModifiedIdentifierSyntax)
+        'Store the collection of declarators
         Dim listOfNewModifiedDeclarators As _
-            New SeparatedSyntaxList(Of VariableDeclaratorSyntax)
+            New SeparatedSyntaxList(
+            Of VariableDeclaratorSyntax)
 
         'Iterate the declarators collection
         For Each declarator In oldDeclarators
             'For each variable name in the declarator...
             For Each modifiedIdentifier In declarator.Names
-                'Get a new proper name
-                Dim tempString = ConvertName(modifiedIdentifier.ToFullString)
+                'Get a new name
+                Dim tempString =
+                    ConvertName(modifiedIdentifier.
+                    ToFullString())
 
                 'Generate a new ModifiedIdentifierSyntax based on
                 'the previous one's properties but with a new Identifier
-                Dim newModifiedIdentifier As ModifiedIdentifierSyntax =
+                Dim newModifiedIdentifier As _
+                    ModifiedIdentifierSyntax =
                     modifiedIdentifier.
-                    WithIdentifier(SyntaxFactory.ParseToken(tempString)).
-                    WithTrailingTrivia(modifiedIdentifier.GetTrailingTrivia)
+                    WithIdentifier(SyntaxFactory.
+                    ParseToken(tempString)).
+                    WithTrailingTrivia(modifiedIdentifier.
+                    GetTrailingTrivia)
 
                 'Add the new element to the collection
                 listOfNewModifiedIdentifiers =
-                    listOfNewModifiedIdentifiers.Add(newModifiedIdentifier)
+                    listOfNewModifiedIdentifiers.
+                    Add(newModifiedIdentifier)
             Next
             'Store a new variable declarator with new
             'variable names
@@ -88,20 +122,29 @@ Friend Class PublicFieldRefactoring_VBCodeRefactoringProvider
                 SeparatedSyntaxList(Of ModifiedIdentifierSyntax)
         Next
 
-
+        'Generate a new FieldDeclarationSyntax
         Dim newField = fieldDeclaration.
             WithDeclarators(listOfNewModifiedDeclarators)
 
+        'Replace the old declaration with the new one
         Dim newRoot As SyntaxNode =
-            root.ReplaceNode(fieldDeclaration, newField)
+            root.ReplaceNode(fieldDeclaration,
+                             newField)
 
+        'Generate a new document
         Dim newDocument =
             document.WithSyntaxRoot(newRoot)
 
+        'Return the new document
         Return newDocument
     End Function
 
-    Private Function ConvertName(oldName As String) As String
-        Return Char.ToUpperInvariant(oldName(0)) + oldName.Substring(1)
+    'Return a new identifier with an
+    'uppercase letter
+    Private Function _
+        ConvertName(oldName As String) As String
+        Return Char.
+            ToUpperInvariant(oldName(0)) &
+            oldName.Substring(1)
     End Function
 End Class
