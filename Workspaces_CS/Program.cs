f7915d26982e5a3e47ec44ab129c5b57f924b1c4
AlessandroDelSole <alessandro.delsole@visual-basic.it>
2015-09-16 14:42:26 +0200

    Renamed sample projects about Emit Added sample projects about Workspaces APIs

---
+++ Workspaces_CS/Program.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Symbols;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Text;
+
+namespace Workspaces_CS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string solutionPath = "C:\\temp\\ConsoleApplication2\\ConsoleApplication2.sln";
+
+            var ws = Microsoft.CodeAnalysis.
+                MSBuild.MSBuildWorkspace.
+                Create();
+
+            var solution = 
+                ws.OpenSolutionAsync(solutionPath).Result;
+
+            IterateSolution(solution, 
+                solutionPath);
+        }
+
+        static void IterateSolution(Solution solution, string solutionPath)
+        {
+            Console.WriteLine(
+                $"Solution {System.IO.Path.GetFileName(solutionPath)}, version {solution.Version.ToString()}");
+
+            foreach (var prj in 
+                     solution.Projects)
+            {
+                Console.WriteLine(
+                    $"Project name: {prj.Name}, version: {prj.Version.ToString()}");
+                Console.WriteLine(
+                    $" {prj.Documents.Count()} code files:");
+
+                foreach (var codeFile in 
+                         prj.Documents)
+                {
+                    Console.
+                    WriteLine($"     {codeFile.Name}");
+                }
+
+                Console.WriteLine(" References:");
+
+                foreach (var reference in 
+                         prj.MetadataReferences)
+                {
+                    Console.WriteLine(
+                        $"    {System.IO.Path.GetFileName(reference.Display)}");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Workspaces_VB/Module1.vb
@@ -0,0 +1,52 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim solutionPath =
+            "C:\temp\ConsoleApplication1\ConsoleApplication1.sln"
+
+        Dim ws =
+            MSBuild.
+            MSBuildWorkspace.Create()
+
+        Dim solution =
+            ws.OpenSolutionAsync(
+            solutionPath).Result
+
+        IterateSolution(
+            solution, solutionPath)
+    End Sub
+
+    Private Sub IterateSolution(
+                solution As Solution,
+                solutionPath As String)
+
+        Console.WriteLine(
+            $"Solution {IO.Path.
+            GetFileName(solutionPath)}, version {solution.Version.ToString}")
+        For Each prj In solution.Projects
+            Console.
+            WriteLine(
+            $"Project name: {prj.Name}, version: {prj.Version.ToString}")
+            Console.
+            WriteLine($" {prj.Documents.Count} code files:")
+
+            For Each codeFile In
+                prj.Documents
+                Console.
+                WriteLine($"     {codeFile.Name}")
+            Next
+
+            Console.
+            WriteLine(" References:")
+
+            For Each ref In
+                prj.MetadataReferences
+                Console.
+                WriteLine($"    {IO.Path.GetFileName(ref.Display)}")
+            Next
+            Console.WriteLine("")
+        Next
+
+        Console.ReadLine()
+    End Sub
+End Module
